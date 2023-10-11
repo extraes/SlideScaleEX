704cb2d72f04ae82ce32be279e12887a4aa72556
extraes <52384576+extraes@users.noreply.github.com>
2023-10-11 01:23:45 -0700

    inc version to 1.3.0 and use proper(?) semver

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using System.Reflection;
 using SlideScale;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(Scale), "SlideScaleEX", "1.2", "Camobiwon, extraes")]
+[assembly: MelonInfo(typeof(Scale), "SlideScaleEX", "1.3.0", "Camobiwon, extraes")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 [assembly: AssemblyTitle("SlideScale")]
 [assembly: AssemblyDescription("Scale items by moving your hands")]
 [assembly: AssemblyCopyright("Camobiwon")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: AssemblyProduct("SlideScale")]
 [assembly: NeutralResourcesLanguage("en")]
